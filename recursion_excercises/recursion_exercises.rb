7ce3cf9a96b6a803f352b7b4fe787ea898360377
Mlkz <maho.mamulashvili@gmail.com>
2022-11-23 14:30:57 -0500

    complete #exponentiation

--- recursion_excercises/recursion_exercises.rb
+++ recursion_excercises/recursion_exercises.rb
@@ -16,3 +16,26 @@ def sum_array(array)
     return array.shift if array.length == 1
     array.shift + sum_array(array)
 end
+
+
+def recursion_1(base, n)
+    return 1 if n == 0
+
+    base * recursion_1(base, n - 1)
+end
+
+recursion_1(1, 0)
+recursion_1(2, 2)
+
+def recursion_2(b, n)
+    return 1 if n == 0
+    return b if n == 1
+
+    if n.even?
+        recursion_1(recursion_2(b, n / 2), 2)
+    else
+        b * recursion_1(recursion_2(b, (n - 1) / 2), 2)
+    end
+end
+
+recursion_2(10, 3)
